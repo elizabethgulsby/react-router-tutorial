8f3291a94c3b3e045126fa5ebd4fcc40c1800f84
Elizabeth Gulsby <elizabeth.gulsby@gmail.com>
2017-01-15 21:24:19 -0500

    Initialized repository

--- lessons/01-setting-up/modules/App.js
+++ lessons/01-setting-up/modules/App.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router'
+import NavLink from './NavLink'
+import Home from './Home'
 
 export default React.createClass({
   render() {
-    return <div>Hello, React Router!</div>
+    return (
+	    <div>
+	    	<h1>React Router Tutorial</h1>
+	    	<ul role="nav">
+	    		<li><NavLink to="/about">About</NavLink></li>
+	    		<li><NavLink to="/repos">Repos</NavLink></li>
+	    	</ul>
+
+	    	<div>
+	    		{this.props.children || <Home />}
+	    	</div>
+	    </div>
+    )
   }
 })
